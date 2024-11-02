3dbfbd1b96f7792c5b142e31c37600ec2d49c514
SifatSararChistee <sifatsararchistee@gamil.com>
2024-11-02 21:02:58 +0600

    added books

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,13 +1,31 @@
-const Book = ({book}) => {
-    const { bookName, author,image,bookId,rating} = book;
-    return (
-        <div>
-            <h1>{bookName}</h1>
-            <div className="w-44">
-            <img src={image} alt="" />
-            </div>
+import { FaRegStar } from "react-icons/fa";
 
+const Book = ({book}) => {
+    const { bookName, author,image,bookId,rating, category} = book;
+    return (
+        
+    <div className="card bg-base-100 w-96 shadow-xl flex items-center p-8 border-2 border-[#13131326]">
+        <figure className="p-10 w-[320px] h-[230px] bg-gray-100 rounded-xl mb-4">
+            <img
+            src={image}
+            alt="Shoes"
+            className="rounded-2xl w-2/3 p-4" />
+        </figure>
+  <div className="flex flex-col items-start w-full">
+    <h2 className="card-title font-extrabold">{bookName}</h2>
+    <p className="text-lg font-medium">By: {author}</p>
+    <div className="flex justify-between w-full">
+    <p className="text-lg font-medium">{category}</p>
+    <div className="flex items-center gap-2">
+    <p className="text-lg font-medium">{rating}</p>
+    <p className="text-lg font-medium"><FaRegStar/></p>
+    </div>
+    
+    </div>
         </div>
+        </div>
+
+
     );
 };
 
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -10,9 +10,9 @@ const Books = () => {
         .then (data=> setBooks(data) )
     },[])
     return (
-        <div>
-            <h1 className="text-5xl">Books</h1>
-            <div className="grid grid-cols-3">
+        <div className="mt-10">
+            <h1 className="text-5xl text-center mb-6">Books</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {
                     books.map(book =><Book key={book.bookId} book={book}></Book>)
                 }
